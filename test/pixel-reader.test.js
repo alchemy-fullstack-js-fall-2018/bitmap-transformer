f72e504d0bda345a3ea9fa19fdb48ab305a364d7
Alex Rankin <alex.c.rankin@gmail.com>
2018-09-28 12:07:08 -0700

    working on pixel reader

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -5,9 +5,6 @@ describe('Pixel Reader', () => {
 
     it('reads pixel from buffer', done => {
         const reader = new PixelReader({ bitsPerPixel: 24 });
-        
-        const colors = [];
-
         // TODO: subscribe to reader "color" event and push into `colors` array.
         // A "color" object should look like:
         // {
@@ -16,16 +13,42 @@ describe('Pixel Reader', () => {
         //     g: <green color value>,
         //     b: <blue color value>,
         // }
+        
+        const colors = [];
+        reader.on('color', color => {
+            colors.push(color);
+        });
+
 
         reader.on('end', () => {
             // write deepEqual assertion for colors versus the
             // expected rgb color objects
 
-            // Don't forget to call done()!
+            
+            assert.deepEqual(colors[0], { offset: 0, b: 0, g: 0, r: 255 });
+            assert.deepEqual(colors[1], { offset: 3, b: 0, g: 255, r: 0 });
+            assert.deepEqual(colors[2], { offset: 6, b: 255, g: 0, r: 0 });
+            
+            done();
         });
 
         // Create a buffer with known data for your colors
-        const buffer = Buffer.alloc(24 * 3); // for three pixels
+        const buffer = Buffer.alloc(24 * 3); 
+        buffer.writeInt8(0xFF, 0);
+        buffer.writeInt8(0x00, 1);
+        buffer.writeInt8(0x00, 2);
+
+        buffer.writeInt8(0x00, 3);
+        buffer.writeInt8(0xFF, 4);
+        buffer.writeInt8(0x00, 5);
+
+        buffer.writeInt8(0x00, 6);
+        buffer.writeInt8(0x00, 7);
+        buffer.writeInt8(0xFF, 8);
+
+
+        
+        // for three pixels
         // TODO: fill buffer with byte values that match your 
         // expected test colors
 
