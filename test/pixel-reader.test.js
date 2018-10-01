540ded8f1fc993e886f99e62d7a3eac9a415b36f
Davidchhing <davidhchhing@gmail.com>
2018-10-01 08:37:45 -0700

    created colors objects array

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -11,7 +11,7 @@ describe('Pixel Reader', () => {
             colors.push(color);
         });
 
-        // TODO: subscribe to reader "color" event and push into `colors` array.
+        // TODONE: subscribe to reader "color" event and push into `colors` array.
         // A "color" object should look like:
         // {
         //     offset: <offset from the start of buffer passed to PixelReader>,
@@ -23,22 +23,45 @@ describe('Pixel Reader', () => {
         reader.on('end', () => {
             // write deepEqual assertion for colors versus the
             // expected rgb color objects
-
             // Don't forget to call done()!
-            done(); 
+            assert.deepEqual(colors, [{
+                offset: 0,
+                b: '80',
+                g: '80',
+                r: '00'
+            }, {
+                offset: 24,
+                b: 'FF',
+                g: '00',
+                r: 'FF'
+            }, {
+                offset: 48,
+                b: '00',
+                g: 'FF',
+                r: '7F'
+            }           
+            ]);
         });
-
+        done(); 
         // Create a buffer with known data for your colors
         const buffer = Buffer.alloc(24 * 3); // for three pixels
         // TODO: fill buffer with byte values that match your 
         // expected test colors
 
         // Call read method with your buffer
-        buffer.writeUInt8(0xFF, 0);
-        buffer.writeUInt8(0xFF, 1);
-        buffer.writeUInt8(0xFF, 2);
+        //this color is teal//
+        buffer.writeUInt8(0x80, 0);
+        buffer.writeUInt8(0x80, 1);
+        buffer.writeUInt8(0x00, 2);
+        //this color is magenta//
+        buffer.writeUInt8(0xFF, 24);
+        buffer.writeUInt8(0x00, 25);
+        buffer.writeUInt8(0xFF, 26);
+        //this color is chartreuse//
+        buffer.writeUInt8(0x00, 48);
+        buffer.writeUInt8(0xFF, 49);
+        buffer.writeUInt8(0x7F, 50);
         reader.read(buffer);
         console.log(buffer);
     });
-
 });
