15d78463a178a7eaad037e25c532e25fdc29e711
Ryan Luras <ryanluras@gmail.com>
2018-10-01 11:56:48 -0700

    all tests passing

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -7,14 +7,6 @@ describe('Pixel Reader', () => {
 
         const reader = new PixelReader({ bitsPerPixel: 24 });
         
-        // TODO: subscribe to reader "color" event and push into `colors` array.
-        // A "color" object should look like:
-        // {
-        //     offset: <offset from the start of buffer passed to PixelReader>,
-        //     r: <red color value>,
-        //     g: <green color value>,
-        //     b: <blue color value>,
-        // }
         const colors = [];
         reader.on('color', color => {
             colors.push(color);
@@ -28,14 +20,10 @@ describe('Pixel Reader', () => {
            
             assert.deepEqual(colors[2], { offset: 6, b:0xFF, g:0x00, r:0x00 });
             done();
-            // write deepEqual assertion for colors versus the
-            // expected rgb color objects
-
-            // Don't forget to call done()!
-
+          
         });
 
-        // Create a buffer with known data for your colors
+      
         const buffer = new Buffer.alloc(24 * 3); 
         
         buffer.writeUInt8(0x00, 0);
