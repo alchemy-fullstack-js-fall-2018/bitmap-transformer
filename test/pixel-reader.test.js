21d24617435d73e7bd8e03984d0bf14a7926b7c9
KLPainter <KLPainter@gmail.com>
2018-09-30 17:03:08 -0700

    pixel reader test working

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -1,33 +1,37 @@
 const assert = require('assert');
-//const PixelReader = require('../lib/pixel-reader');
+const PixelReader = require('../lib/pixel-reader');
 
 describe('Pixel Reader', () => {
 
-    it.skip('reads pixel from buffer', done => {
+    it('reads pixel from buffer', done => {
         const reader = new PixelReader({ bitsPerPixel: 24 });
+
+        const purplePixel = { offset: 0, r: 127, g: 0, b: 255 };
+        const yellowPixel = { offset: 3, r: 255, g: 255, b: 51 };
+        const greenPixel = { offset: 6,  r: 102, g: 204, b: 0 };
         
         const colors = [];
-
-        // TODO: subscribe to reader "color" event and push into `colors` array.
-        // A "color" object should look like:
-        // {
-        //     offset: <offset from the start of buffer passed to PixelReader>,
-        //     r: <red color value>,
-        //     g: <green color value>,
-        //     b: <blue color value>,
-        // }
+        reader.on('color', (color) => {
+            colors.push(color);
+        });
 
         reader.on('end', () => {
-            // write deepEqual assertion for colors versus the
-            // expected rgb color objects
-
-            // Don't forget to call done()!
+            const expected = [purplePixel, yellowPixel, greenPixel];
+            assert.deepEqual (colors, expected);
+            done();
         });
 
         // Create a buffer with known data for your colors
-        const buffer = Buffer.alloc(24 * 3); // for three pixels
-        // TODO: fill buffer with byte values that match your 
-        // expected test colors
+        const buffer = Buffer.alloc(3 * 3); // for three pixels
+        buffer.writeUInt8(purplePixel.b, 0);
+        buffer.writeUInt8(purplePixel.g, 1);
+        buffer.writeUInt8(purplePixel.r, 2);
+        buffer.writeUInt8(yellowPixel.b, 3);
+        buffer.writeUInt8(yellowPixel.g, 4);
+        buffer.writeUInt8(yellowPixel.r, 5);
+        buffer.writeUInt8(greenPixel.b, 6);
+        buffer.writeUInt8(greenPixel.g, 7);
+        buffer.writeUInt8(greenPixel.r, 8);
 
         // Call read method with your buffer
         reader.read(buffer);
